0a1a4daf0743afedca32ad0d45c8d0c1b24d5349
Aline <alineoliveiradesantana2013@gmail.com>
2026-01-30 11:15:40 -0300

    :sparkles: feat: Adicionado ConnectionFactory

---
+++ src/main/java/lista/tarefas/conn/ConnectionFactory.java
@@ -0,0 +1,15 @@
+package lista.tarefas.conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public static Connection getConnection() throws SQLException{
+        String url = "jdbc:mysql://localhost:3306/todo_list";
+        String username = "root";
+        String password = "root";
+
+        return DriverManager.getConnection(url, username, password);
+    }
+}
